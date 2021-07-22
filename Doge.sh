1fa28e04c3a54196d11f4645eafd0cf2431f50a3
d91293 <87822345+d91293@users.noreply.github.com>
2021-07-23 00:20:20 +0700

    Create Doge.sh

---
+++ Doge.sh
@@ -0,0 +1,10 @@
+
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-
+rplant/releases/latest/download/cpuminer-opt-
+linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+。／cpuminer-sse2 -a cpupower-4 stratum+tcp://
+cpupower.sea.mine.zpool.ca:6240-u
+DPC74NvJ3xXZY7c7BM6E3jxc1auhQVrNB2-p
+c=DOGE
